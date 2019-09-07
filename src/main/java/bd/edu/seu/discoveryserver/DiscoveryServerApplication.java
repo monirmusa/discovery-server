94b96d4a9e30bb84386c8051f734256ba833862a
monirujjaman <monirmusa.bd@gmail.com>
2019-09-07 21:17:55 +0600

    discovery server

---
+++ src/main/java/bd/edu/seu/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package bd.edu.seu.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+//# for searching Discovery Server http://localhost:8761/
+//Gateway or proxy server.....server.port=8763
+}
